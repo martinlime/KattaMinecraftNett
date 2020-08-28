60683e51957ae7cc2a2378b64b5c851de7f11a37
Eiromplays <26148920+Eiromplays@users.noreply.github.com>
2020-08-28 13:33:26 +0200

    Fiks og nye ting

--- script.js
+++ script.js
@@ -1,12 +1,16 @@
-function Clearpicture() {
-    var img = document.getElementById("clearImage");
-    if (img) {
-        if (img.style.display === "block") {
-            img.style.display = "none";
+function Clearpicture(img) {
+    var center = document.getElementById("clearImageText");
+    if (center && img) {
+        if (img.style.opacity === "0.3") {
+            img.style.opacity = 1;
+            document.getElementById("showText").remove();
         } else {
-            img.style.display = "block";
+            img.style.opacity = 0.3;
+            var P = document.createElement("P");
+            P.innerHTML = "TEst";
+            P.id = "showText";
+            P.style = "margin-top: -225px";
+            center.appendChild(P);
         }
-    } else {
-        alert("Not found");
     }
 }
